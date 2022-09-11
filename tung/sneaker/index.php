58b61d428df61aad329fae4a68d714731ea88868
npctung <tungtrinhthanh0311@gmail.com>
2022-09-12 00:01:35 +0700

    first commit

---
+++ tung/sneaker/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trang chủ</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/css/components/Adidas/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"crossorigin="anonymous"></script>
+</head>
+<body>
+    <div>
+    <div class="header">
+        <div class="header-body">
+            <div class="logo">
+                <a href="index.php">ở đây để logo</a>
+            </div>
+            <div class="search">
+                <div class="search-imgs">
+                    <i class="bi bi-search"></i>
+                </div>
+                <div class="search-img">
+                    <form action="">
+                        <input type="search" class="search-input" placeholder="   Nhập tên sản phẩm và enter..." />
+                    </form>
+                </div>
+            </div>
+            <div class="map">
+                <div class="img">
+                    <i class="bi bi-geo-alt-fill"></i>
+                    <a href="#">235 Hoàng Quốc Việt, Cổ Nhuế, Bắc Từ Liêm, Hà Nội</a>
+                </div>
+            </div>
+            <div class="hotline">
+                <p>Hotline <br/>
+                    0438592183
+                </p>
+                <div class="zalo"><p>Zalo</p></div>
+            </div>
+            <div class="cart">
+                <i class="bi bi-cart4"></i>
+            </div>
+            <div class="login">
+                <div class="people">
+                    <i class="bi bi-person-circle"></i>
+                </div>
+                <div class="bnt">
+                    <form action="">
+                        <input type="submit" class="login-bnt" value="Đăng nhập">
+                    </form>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="list">
+                <ul class="list-ul">
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="tmp/Adidas/Adidas-index.php"> Adidas </a>
+                            <div class="dropdown-content">
+                              <a href="tmp/Adidas/components/SuperStar/SuperStar-index.html">Adidas SuperStar</a>
+                              <a href="tmp/Adidas/components/Stan Smith/Stan-Smith-index.html">Adidas Stan Smith</a>
+                              <a href="tmp/Adidas/components/Alphabounce/Alphabounce-index.html">Adidas Alphabounce</a>
+                              <a href="tmp/Adidas/components/NMD/NMD-index.html">Adidas NMD</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="tmp/Nike/Nike-index.php"> Nike </a>
+                            <div class="dropdown-content">
+                              <a href="tmp/Nike/components/Air Force 1/Air-Force-1-index.html">Nike Air Force 1</a>
+                              <a href="tmp/Nike/components/Jordan/Jordan-index.html">Nike Jordan</a>
+                              <a href="tmp/Nike/components/Air Max/Air-Max-index.html">Nike Air Max</a>
+                              <a href="tmp/Nike/components/M2K Tekno/M2K-Tekno-index.html">Nike M2K Tekno</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="#">New Balance</a>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="#"> MLB </a>
+                            <div class="dropdown-content" aria-labelledby="dropdownMenuLink">
+                              <a href="#">Adidas SuperStar</a>
+                              <a href="#">Adidas Stan Smith</a>
+                              <a href="#">Adidas Alphabounce</a>
+                              <a href="#">Adidas NMD</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="#"> Balenciaga </a>
+                            <div class="dropdown-content" aria-labelledby="dropdownMenuLink">
+                              <a href="#">Adidas SuperStar</a>
+                              <a href="#">Adidas Stan Smith</a>
+                              <a href="#">Adidas Alphabounce</a>
+                              <a href="#">Adidas NMD</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="list-li">
+                        <div class="dropdown">
+                            <a class="dropbtn" href="#"> Converse </a>
+                            <div class="dropdown-content" aria-labelledby="dropdownMenuLink">
+                              <a href="#">Adidas SuperStar</a>
+                              <a href="#">Adidas Stan Smith</a>
+                              <a href="#">Adidas Alphabounce</a>
+                              <a href="#">Adidas NMD</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div><br>
+    </div>
+</body>
+</html>
